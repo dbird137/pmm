ca389dd8d314700cd57542d77818eb8c2c2c05d3
dbird137 <27447452+dbird137@users.noreply.github.com>
2019-03-20 17:32:05 -0700

    fixing custom branch paths

--- pmm.cmake
+++ pmm.cmake
@@ -34,7 +34,7 @@ endmacro()
 # The version:
 _pmm_set_if_undef(PMM_VERSION ${PMM_VERSION_INIT})
 # The base URL we download PMM from:
-_pmm_set_if_undef(PMM_URL_BASE "https://vector-of-bool.github.io/pmm")
+_pmm_set_if_undef(PMM_URL_BASE "https://github.com/dbird137/pmm")
 # The real URL we download from (Based on the version)
 _pmm_set_if_undef(PMM_URL "${PMM_URL_BASE}/${PMM_VERSION}")
 # The directory where we store our downloaded files
